f6882918fdb9a9c5ce0d7d6f60e953f3a8286b9d
YangSai <yangsai198742@163.com>
2016-02-22 10:43:30 +0800

    添加注释

--- OKCycleScroll/OKCycleScroll/OKCycleScrollView/OKCycleScrollView.h
+++ OKCycleScroll/OKCycleScroll/OKCycleScrollView/OKCycleScrollView.h
@@ -22,7 +22,16 @@
 @property (nonatomic, assign) id<OKCycleScrollViewDelegate> delegate;
 
 
-
+/**
+ *  添加轮播图到指定视图上
+ *
+ *  @param view                         指定视图
+ *  @param frame                        在视图上的Frame
+ *  @param delegate                     设置代理
+ *  @param images                       加载的图片
+ *
+ *  @return 轮播图实例对象
+ */
 + (instancetype)createOKCycleScrollViewInView:(UIView* )view Frame:(CGRect)frame delegate:(id<OKCycleScrollViewDelegate>)delegate images:(NSArray*) images;
 
 
